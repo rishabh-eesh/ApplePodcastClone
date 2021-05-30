0e705fda225a6112769b62b737be1de849a64af1
Rishabh Dubey <rishabhdubey@Rishabhs-MacBook-Air.local>
2021-05-30 20:52:55 +0530

    Initial commit

---
+++ ApplePodcastClone/Presentations/Controllers/MainTabBarController.swift
@@ -0,0 +1,46 @@
+//
+//  MainTabBarController.swift
+//  ApplePodcastClone
+//
+//  Created by Rishabh Dubey on 29/05/21.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = .purple
+        
+        setupViewController()
+    }
+    
+    // MARK: - TabBar ViewControllers Setup
+    fileprivate func setupViewController() {
+        viewControllers = [
+            createTabBarController(viewController: ViewController(), title: "Favorites", imageName: TabBarIcon.favorites.rawValue),
+            createTabBarController(viewController: ViewController(), title: "Search", imageName: TabBarIcon.search.rawValue),
+            createTabBarController(viewController: ViewController(), title: "Downloads", imageName: TabBarIcon.downloads.rawValue)
+        ]
+    }
+    
+    // MARK:- Helper function
+    fileprivate func createTabBarController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .white
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        return navController
+    }
+}
+
+// MARK: TabBar icons
+enum TabBarIcon: String {
+    case favorites
+    case search
+    case downloads
+}
---
+++ ApplePodcastClone/Presentations/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePodcastClone
+//
+//  Created by Rishabh Dubey on 29/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
